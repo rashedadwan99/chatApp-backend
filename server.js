697509978d430401c573cf1690993a56b422d85a
rashedadwan99 <rashedadwan56789@gmail.com>
2022-12-19 20:18:04 +0300

    remove path.resolve

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 require("colors");
 const app = express();
 require("./startup/db")();
+require("./startup/cors")(app)
 require("./startup/routes")(app);
 
 app.use(notFound);
 app.use(errorHandler);
-
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`.yellow.bold);
