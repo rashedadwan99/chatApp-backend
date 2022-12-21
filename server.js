4f65d62e48055f7dfeca9da25bbe09832fa80bf9
rashedadwan99 <rashedadwan56789@gmail.com>
2022-12-21 16:29:25 +0300

    solving the problem of not found

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require("./startup/routes")(app);
 
 app.use(notFound);
 app.use(errorHandler);
-app.use(express.static())
+// app.use(express.static(""))
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`.yellow.bold);
