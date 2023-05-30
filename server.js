47f9e8d96613205649bea1226a6d2d6c0a6463e9
rashedadwan99 <rashedadwan56789@gmail.com>
2023-05-30 18:47:32 +0300

    update package json

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`.yellow.bold);
 });
+
 const io = require("socket.io")(server, {
   pingTimeOut: 60000,
   cors: {
